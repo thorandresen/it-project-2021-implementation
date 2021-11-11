aee68b7aebc774226ecd11a56bab87ffc8674e86
Mathias <matti-drengen@hotmail.com>
2021-11-11 16:03:58 +0100

    android init push

---
+++ Android/app/src/main/java/com/example/verifyr/StubServer.java
@@ -0,0 +1,28 @@
+package com.example.verifyr;
+
+import android.content.Context;
+
+import java.util.HashMap;
+
+public class StubServer implements Server{
+
+    private final int pufId;
+
+    public StubServer(Context context, int pufId){
+        this.pufId = pufId;
+    }
+    @Override
+    public void getChallenge(int pufID,final ChallengeVolleyCallback callback) {
+        callback.onSuccess(pufID*4);
+    }
+
+    @Override
+    public void verify(int pufID, int challenge, int response, VerifyVolleyCallback callback) {
+        if(response==challenge/2){
+            callback.onSuccess(true);
+        }else{
+            callback.onSuccess(false);
+        }
+    }
+
+}
---
+++ Android/app/src/main/java/com/example/verifyr/VerifyVolleyCallback.java
@@ -0,0 +1,5 @@
+package com.example.verifyr;
+
+public interface VerifyVolleyCallback {
+    public void onSuccess(boolean verified);
+}
