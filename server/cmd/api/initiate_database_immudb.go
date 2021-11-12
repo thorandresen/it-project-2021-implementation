6772e9ef9fb639fd40c9693703d8d34808cf96f1
Anders <anders.wiggers@gmail.com>
2021-11-12 10:58:51 +0100

    one time challenge verification

--- server/cmd/api/initiate_database_immudb.go
+++ server/cmd/api/initiate_database_immudb.go
@@ -8,19 +8,25 @@ import (
 	"github.com/codenotary/immudb/pkg/stdlib"
 )
 
-
 func initiateDatabase(ServerConfig ServerConfig) {
+	// SQL Commands for the database initiatization -- Create tables for users and devices
+	var commands []string
+	commands = append(commands, "CREATE TABLE IF NOT EXISTS devices(pid INTEGER, owner VARCHAR, challenge_counter INTEGER, state VARCHAR, PRIMARY KEY pid);")
+	commands = append(commands, "CREATE TABLE IF NOT EXISTS users(id VARCHAR[256], pk VARCHAR, first_name VARCHAR, last_name VARCHAR, phone_number INTEGER, email VARCHAR, PRIMARY KEY id)")
+
+	// Connect to db
 	opts := client.DefaultOptions()
-	opts.Username = "immudb"
-	opts.Password = "immudb"
+	opts.Username = ServerConfig.db_username
+	opts.Password = ServerConfig.db_password
 	opts.Database = "defaultdb"
 	opts.Address = ServerConfig.db_addr
 	opts.Port = ServerConfig.db_port
 	
 	db := stdlib.OpenDB(opts)
 	defer db.Close()
-	
 
-	_, _ = db.ExecContext(context.TODO(), "CREATE TABLE devices(pid INTEGER, owner VARCHAR, challenge_counter INTEGER, state VARCHAR, PRIMARY KEY pid);")
-	_, _ = db.ExecContext(context.TODO(), "CREATE TABLE users(id VARCHAR, token VARCHAR, pk VARCHAR, name VARCHAR, number INTEGER, email VARCHAR)")
+	//Run through commands 
+	for _, command := range commands{
+		_, _ = db.ExecContext(context.TODO(), command)
+	}
 }
