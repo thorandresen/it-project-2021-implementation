60fc13e96db926f642c36ecd6b7255c189032b45
Mathias <matti-drengen@hotmail.com>
2022-01-10 12:24:34 +0100

    script updates

--- script.go
+++ script.go
@@ -14,6 +14,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -64,33 +65,30 @@ func verify(c int, pufId int, r string) (v bool) {
 	}
 	return
 }
-func execute(pufId int) {
-	var times []time.Duration
-	var counter = 0
+func execute(pufId int, m *sync.Mutex) {
+	var times []int64
+	// var counter = 0
 	for i := 0; i < 1000; i++ {
 		start := time.Now()
 		var c = getChallenge(pufId)
 		var r = calcResponse(c, pufId)
 		verify(c, pufId, r)
-		elapsed := time.Since(start)
+		elapsed := time.Since(start).Milliseconds()
 		times = append(times, elapsed)
-		counter++
-		if counter%100 == 0 {
-			fmt.Println("_________________________________________________________________________________")
-			fmt.Println(times)
-		}
+		// counter++
+		// if counter%100 == 0 {
+		// 	fmt.Println("_________________________________________________________________________________")
+		// 	fmt.Println(times)
+		// }
 	}
+	logTimes(times, m)
+
 }
 
-func requestOwnership() (v bool) {
-	signingString := "skrt"
+func requestOwnership(pufId int, m *sync.Mutex) (v bool) {
+	signingString := strconv.Itoa(pufId)
 
 	sk, err := rsa.GenerateKey(rand.Reader, 2048)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 
 	if err != nil {
 		fmt.Println(err)
@@ -105,48 +103,81 @@ func requestOwnership() (v bool) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(sigBytes)
+	// pk := &sk.PublicKey
+	// fmt.Print(pk)
+
+	// fmt.Println(sigBytes)
 
 	data, _ := json.Marshal(map[string]string{
-		"sig": string(sigBytes),
-		"bid": "1",
+		"signature": string(sigBytes),
+		"uuid":      "sad",
+		"pufId":     strconv.Itoa(pufId),
 	})
 
-	resp, err := http.Post("https://ta.anrs.dk/transfer", "application/json", bytes.NewBuffer(data))
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	v, err = strconv.ParseBool(string(body))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return
+	var times []int64
+	// var counter = 0
+	for i := 0; i < 1000; i++ {
+		start := time.Now()
 
-	// Verify
+		resp, err := http.Post("https://ta.anrs.dk/transfer/request", "application/json", bytes.NewBuffer(data))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if resp.StatusCode == 200 || resp.StatusCode == 401 {
+			elapsed := time.Since(start).Milliseconds()
+			times = append(times, elapsed)
+			// counter++
+			// if counter%100 == 0 {
+			// 	fmt.Println("_________________________________________________________________________________")
+			// 	fmt.Println(times)
+			// }
+		}
+	}
+	logTimes(times, m)
+	// //Verify
 	// pk := &sk.PublicKey
 	// h := sha256.New()
-	// h.Write([]byte("skr"))
+	// h.Write([]byte("2"))
 	// d := h.Sum(nil)
 	// if rsa.VerifyPSS(pk, crypto.SHA256, d, sigBytes, nil) == nil {
 	// 	fmt.Print("Skkkrrtttt")
 	// } else {
 	// 	fmt.Print("pahhhh :(")
 	// }
+
+	return
+}
+
+func logTimes(times []int64, m *sync.Mutex) {
+	m.Lock()
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+	log.Println(times)
+	sum := int64(0)
+	for i := 0; i < len(times); i++ {
+		sum += times[i]
+	}
+	avg := sum / 1000
+	log.Println("Average: ")
+	log.Print(avg)
+	log.Println("___________________________")
+
+	m.Unlock()
 }
 
 // Main function
 func main() {
-	// var pufId = 2
-	// for i := 0; i < 4; i++ {
-	// 	go execute(pufId)
-	// }
-	// for true {
-	// }
-	requestOwnership()
+	var m sync.Mutex
+
+	var pufId = 1
+	for i := 0; i < 4; i++ {
+		// go execute(pufId)
+		go requestOwnership(pufId, &m)
+	}
+	for true {
+	}
 
 }
